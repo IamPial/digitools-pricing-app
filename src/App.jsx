20bc166cfee33b81010e1ed2393e8f485a074fcd
Pial <pialname394@gmail.com>
2026-03-30 01:23:40 +0600

    initialize this projects

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" flex justify-center ">
+      <h1 className="btn  btn-success  py-10 text-slate-800 font-bold text-4xl mt-5">
+        Hello App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
