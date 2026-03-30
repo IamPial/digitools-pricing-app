c8018646af676fc3066615c4fc4bc1dfbb2cd97c
Pial <pialname394@gmail.com>
2026-03-30 23:41:15 +0600

    added counter section the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Homepage/Banner";
+import Counter from "./components/Homepage/Counter";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Counter />
     </div>
   );
 };
