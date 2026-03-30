d483a1db31ae82e9da6c6487264574f8d27948ea
Pial <pialname394@gmail.com>
2026-03-30 12:06:39 +0600

    create navbar list items according to figma

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,21 +29,19 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Item 1</a>
+                <a href="">Products</a>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <a href="">Features</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a href="">Pricing</a>
+              </li>
+              <li>
+                <a href="">Testimonials</a>
+              </li>
+              <li>
+                <a href="">FAQ</a>
               </li>
             </ul>
           </div>
@@ -54,23 +52,19 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <a href="">Products</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a href="">Features</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a href="">Pricing</a>
+            </li>
+            <li>
+              <a href="">Testimonials</a>
+            </li>
+            <li>
+              <a href="">FAQ</a>
             </li>
           </ul>
         </div>
