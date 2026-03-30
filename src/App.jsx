6599b78b468aa6804a125fa857c177d5e5f86787
Pial <pialname394@gmail.com>
2026-03-30 12:03:21 +0600

    added navbar components to the this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <div className=" flex justify-center ">
-      <h1 className="btn  btn-success  py-10 text-slate-800 font-bold text-4xl mt-5">
-        Hello App
-      </h1>
+    <div>
+      <Navbar />
     </div>
   );
 };
