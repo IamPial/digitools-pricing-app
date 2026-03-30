0ac91897ae37050581c2e61e52185aabf79d0ab8
Pial <pialname394@gmail.com>
2026-03-30 23:01:57 +0600

    added toggling to the explore btn

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import { CiPlay1 } from "react-icons/ci";
 import statusImg from "../../assets/status.png";
 import bannerImg from "../../assets/banner.png";
 
 const Banner = () => {
+  const [toggleBtn, setToggleBtn] = useState("explore");
   return (
     <div className="container mx-auto py-20">
       <div className="grid grid-cols-1 lg:grid-cols-2 p-2 lg:p-0  gap-10 lg:gap-2  items-center">
@@ -24,11 +26,14 @@ const Banner = () => {
             Explore Products
           </p>
           <div className="flex gap-4">
-            <button className="btn rounded-full p-6 text-white text-[16px] font-bold bg-linear-to-r from-[#4f39f6] to-[#9514fa]">
+            <button
+              onClick={() => setToggleBtn("explore")}
+              className={`btn rounded-full p-6  text-purple-500 text-[16px] font-bold ${toggleBtn === "explore" ? "bg-linear-to-r from-[#4f39f6] to-[#9514fa] text-white" : "bg-white border border-purple-500 "}`}
+            >
               Explore Products
             </button>
             <div className="bg-linear-to-r border from-[#4f39f6] to-[#9514fa] bg-clip-border border-transparent inline-block rounded-full">
-              <button className="btn rounded-full p-6 bg-white ">
+              <button className="btn rounded-full p-6 bg-white">
                 {" "}
                 <CiPlay1 className="text-purple-800 text-xl " />
                 <span className="font-medium text-[16px] bg-linear-to-r from-[#4f39f6]  to-[#9514fa]  bg-clip-text text-transparent">
