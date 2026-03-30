b33c1751b2df0bc3a8b9e7a6053371abf042ec43
Pial <pialname394@gmail.com>
2026-03-31 02:44:43 +0600

    added responsiveness for every devices

--- src/components/Features/MyProducts/MyProducts.jsx
+++ src/components/Features/MyProducts/MyProducts.jsx
@@ -8,7 +8,7 @@ const MyProducts = ({ fetchPromise }) => {
   console.log(productData);
   return (
     <div>
-      <div className=" grid grid-cols-3 gap-7 pt-10">
+      <div className=" grid grid-cols-1  px-4 md:px-0 md:grid-cols-2 lg:grid-cols-3 gap-7 pt-10">
         {productData.map((product) => {
           return <MyProductCard product={product} key={product.id} />;
         })}
