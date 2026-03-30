23fa772820a21b47ba24fb929844e16d70c876c8
Pial <pialname394@gmail.com>
2026-03-31 01:12:17 +0600

    added json data & pass it with props

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import Banner from "./components/Homepage/Banner";
 import Counter from "./components/Homepage/Counter";
 import Features from "./components/Features/Features";
 
+const fetchData = async () => {
+  const res = await fetch("/products.json");
+  return res.json();
+};
+const fetchPromise = fetchData();
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Banner />
       <Counter />
-      <Features />
+      <Features fetchPromise={fetchPromise} />
     </div>
   );
 };
