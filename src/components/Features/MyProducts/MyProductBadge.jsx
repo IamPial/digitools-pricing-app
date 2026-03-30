54f606fcdffbfdef28474bc3150452b05769dae0
Pial <pialname394@gmail.com>
2026-03-31 02:28:30 +0600

    create a status components

---
+++ src/components/Features/MyProducts/MyProductBadge.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MyProductBadge = ({ product }) => {
+  return (
+    <>
+      {product.tagType === "new" ? (
+        <span className="badge bg-[#DBFCE7] text-[#0A883E] font-medium text-sm px-3 py-1.5 rounded-full capitalize">
+          {product.tagType}
+        </span>
+      ) : product.tagType === "best seller" ? (
+        <span className="badge bg-[#FEF3C6] text-[#BB4D00] font-medium text-sm px-3 py-1.5 rounded-full capitalize">
+          {product.tagType}
+        </span>
+      ) : (
+        <span className="badge bg-[#E1E7FF] text-[#4f39f6] font-medium text-sm px-3 py-1.5 rounded-full capitalize">
+          {product.tagType}
+        </span>
+      )}
+    </>
+  );
+};
+
+export default MyProductBadge;
