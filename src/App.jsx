8a610813a0bb1a5fe228d250d855d3290fd45265
Pial <pialname394@gmail.com>
2026-03-30 17:41:53 +0600

    added Banner section to App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Homepage/Banner";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
   );
 };
