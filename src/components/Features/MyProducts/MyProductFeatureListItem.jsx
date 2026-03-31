c017234139204988915c198a121cd788b8823389
Pial <pialname394@gmail.com>
2026-03-31 14:52:14 +0600

    added the types of props with proptypes

--- src/components/Features/MyProducts/MyProductFeatureListItem.jsx
+++ src/components/Features/MyProducts/MyProductFeatureListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { FaCheck } from "react-icons/fa6";
 const MyProductFeatureListItem = ({ index, feature }) => {
   return (
@@ -8,6 +9,11 @@ const MyProductFeatureListItem = ({ index, feature }) => {
       </span>
     </li>
   );
+};
+
+MyProductFeatureListItem.propTypes = {
+  index: PropTypes.number,
+  features: PropTypes.array,
 };
 
 export default MyProductFeatureListItem;
