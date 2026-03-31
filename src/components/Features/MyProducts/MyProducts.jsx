06bcbcdf08b0fd3a377a0113608355e0b72e6d9c
Pial <pialname394@gmail.com>
2026-03-31 12:37:21 +0600

    passing props

--- src/components/Features/MyProducts/MyProducts.jsx
+++ src/components/Features/MyProducts/MyProducts.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { use } from "react";
 
 import MyProductCard from "./MyProductCard";
 
-const MyProducts = ({ fetchPromise }) => {
-  const productData = use(fetchPromise);
+const MyProducts = ({ data }) => {
+  // const productData = use(fetchPromise);
 
   return (
     <div>
       <div className=" grid grid-cols-1  px-4 md:px-0 md:grid-cols-2 lg:grid-cols-3 gap-7 pt-10">
-        {productData.map((product) => {
+        {data.map((product) => {
           return <MyProductCard product={product} key={product.id} />;
         })}
       </div>
