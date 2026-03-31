a81e5bf4b204be6ec1078670b88cd307473b483b
Pial <pialname394@gmail.com>
2026-03-31 12:09:58 +0600

    remove logs from this file

--- src/components/Features/MyProducts/MyProducts.jsx
+++ src/components/Features/MyProducts/MyProducts.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { use } from "react";
-import { FaCheck } from "react-icons/fa6";
+
 import MyProductCard from "./MyProductCard";
 
 const MyProducts = ({ fetchPromise }) => {
   const productData = use(fetchPromise);
-  console.log(productData);
+
   return (
     <div>
       <div className=" grid grid-cols-1  px-4 md:px-0 md:grid-cols-2 lg:grid-cols-3 gap-7 pt-10">
