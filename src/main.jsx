e57a0ddb250f61cf5b94a23fe5fb139b29829639
Pial <pialname394@gmail.com>
2026-03-31 23:51:59 +0600

    import toastContainer here for toasting message

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+    <ToastContainer autoClose={3000} />
+  </StrictMode>,
+);
