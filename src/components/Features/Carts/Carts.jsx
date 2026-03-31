9e74db60bfef6c3acc3c5c4de27112b009b06c3b
Pial <pialname394@gmail.com>
2026-03-31 16:19:00 +0600

    render the carts ui with conditions

--- src/components/Features/Carts/Carts.jsx
+++ src/components/Features/Carts/Carts.jsx
@@ -1,48 +1,58 @@
 import React from "react";
+import PropTypes from "prop-types";
+import CartsCard from "./CartsCard";
+import cartImg from "../../../assets/shopping-cart-img.png";
 
-const Carts = ({ data }) => {
+const Carts = ({ buyItem, setBuyItem }) => {
+  const handleDelete = (obj) => {
+    console.log(obj);
+  };
   return (
     <div className="pt-10 max-w-180 mx-auto">
       <div className=" border border-gray-200 rounded-xl p-10">
         <h2 className="text-2xl font-bold text-slate-900 mb-4">Your Cart</h2>
         <div className="space-y-4">
-          {data.map((cart) => {
-            return (
-              <div key={cart.id} className="flex gap-4 flex-col">
-                <div className="flex  items-center justify-between   p-4">
-                  <div className="flex gap-4 items-center">
-                    <div className="w-12 h-12 rounded-full border border-gray-200 ">
-                      <img
-                        className="block mx-auto mt-2"
-                        src={cart.icon}
-                        alt={cart.name}
-                      />
-                    </div>
-                    <div>
-                      <h2 className="font-semibold text-xl text-slate-900">
-                        {cart.name}
-                      </h2>
-                      <p className="text-[16px] text-slate-500 font-medium">
-                        ${cart.price}
-                      </p>
-                    </div>
-                  </div>
-                  <button className="btn btn-error btn-soft">Remove</button>
-                </div>
+          {buyItem.length === 0 ? (
+            <div className="flex justify-center items-center flex-col gap-5">
+              <img className="w-20" src={cartImg} alt="" />
+              <h2 className="font-bold text-2xl text-slate-500">
+                Your cart is Empty
+              </h2>
+            </div>
+          ) : (
+            buyItem.map((cart) => {
+              return (
+                <CartsCard
+                  key={cart.id}
+                  cart={cart}
+                  handleDelete={handleDelete}
+                />
+              );
+            })
+          )}
+        </div>
+        <div className="space-y-4">
+          {buyItem.length === 0 ? (
+            ""
+          ) : (
+            <div>
+              <div className="flex justify-between items-center">
+                <span className="text-[16px] text-slate-500">Total:</span>
+                <span className="text-2xl font-bold text-slate-900">$70</span>
               </div>
-            );
-          })}
-          <div className="flex justify-between items-center">
-            <span className="text-[16px] text-slate-500">Total:</span>
-            <span className="text-2xl font-bold text-slate-900">$70</span>
-          </div>
-          <button className="w-full rounded-full text-[16px] text-white btn bg-linear-to-r from-[#4f39f9] to-[#9514fa]">
-            Proceed to Checkout
-          </button>
+              <button className="w-full rounded-full text-[16px] text-white btn bg-linear-to-r from-[#4f39f9] to-[#9514fa]">
+                Proceed to Checkout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
+};
+
+Carts.propTypes = {
+  buyItem: PropTypes.array.isRequired,
 };
 
 export default Carts;
