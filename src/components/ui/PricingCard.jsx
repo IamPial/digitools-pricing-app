c687d3c05866730a395c4a2ae55fa064a01e7459
Pial <pialname394@gmail.com>
2026-04-01 03:00:25 +0600

    responsiveness added to the badge

--- src/components/ui/PricingCard.jsx
+++ src/components/ui/PricingCard.jsx
@@ -6,7 +6,7 @@ const PricingCard = ({ pricingList }) => {
     <div>
       {pricingList.isPopular === true ? (
         <div className=" relative -z-1 select-none border bg-linear-to-r from-[#4f39f6] to-[#9514fa] border-gray-200 rounded-xl p-6">
-          <span className=" absolute  -top-4 left-40  sm:left-60 md:left-26 xl:left-36 p-4 badge bg-[#FEF3C6] border-none text-[#BB4D00] font-medium rounded-full">
+          <span className=" absolute  -top-4 left-40  sm:left-60 md:left-26 xl:left-36 2xl:left-45 p-4 badge bg-[#FEF3C6] border-none text-[#BB4D00] font-medium rounded-full">
             {pricingList.tag}
           </span>
           <div className="h-94">
