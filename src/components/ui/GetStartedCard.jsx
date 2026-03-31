ddcfb705d3705432d3c1bbee7f4e0e550cc37b95
Pial <pialname394@gmail.com>
2026-04-01 00:54:53 +0600

    created getStartedCard for reusable

---
+++ src/components/ui/GetStartedCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const GetStartedCard = ({ title, description, cardImg, value }) => {
+  return (
+    <div>
+      <div className="border border-gray-200 rounded-xl p-6  ">
+        <div className="flex justify-end">
+          <span className="badge bg-linear-to-r from-[#4f39f6] to-[#9514fa] rounded-full w-10 h-10 p-1 text-white">
+            {value}
+          </span>
+        </div>
+        <div className="py-20">
+          <div className="mx-auto w-25 h-25 rounded-full bg-[#E1E7FF] mb-4">
+            <img className="block mx-auto p-4" src={cardImg} alt="" />
+          </div>
+          <div className="text-center space-y-4">
+            <h2 className="text-2xl font-bold text-slate-900">{title}</h2>
+            <p className="text-[16px] text-slate-500 leading-5">
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetStartedCard;
